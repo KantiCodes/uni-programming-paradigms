fe1faac3deef5e1dee7ed87c40d0de0edb06f0d6
eviluser <jendrekpb@gmail.com>
2022-01-03 23:28:08 +0100

    Solved datalog and prolog exercises

---
+++ prolog/session11.pl
@@ -0,0 +1,42 @@
+#lang racklog
+
+nat(zero).
+nat(succ(X)) :- nat(X).
+
+
+fib(0,1).
+fib(1,1).
+fib(X,Z) :- N1 is X-1, N2 is X-2, fib(N1,Z1), fib(N2, Z2), Z is Z1+Z2.
+
+
+% Problem 4 
+
+prefixx([],_).
+prefixx([X|XS],[X|YS]) :- prefixx(XS,YS).
+
+%prefix [] [] = True
+%prefix (x:xs) (y:ys) = if x==y then prefix xs ys
+
+% Problem 5
+
+double([],[]).
+double([X|A],[X|[X|B]]) :- double(A,B).
+
+tripple([], []).
+tripple([X|A],[X|[X|[X|B]]]) :- tripple(A,B).
+
+% Problem 6
+
+zip([],[],[]).
+zip([X|XS],[Y|YS],[(X,Y)|XSYS]) :- zip(XS,YS,XSYS).
+
+unzip([],[],[]).
+unzip([(X,Y)|XSYS],[X|XS],[Y|YS]) :- unzip(XSYS,XS,YS).
+
+% Problem 7
+
+append1([],YS,YS).
+append1([X|XS],YS,[X|XSYS]) :- append1(XS,YS,XSYS).
+
+prefix1(X,Y) :- append1(X,_,Y).
+suffix1(X,Y) :- append1(_,X,Y).
---
+++ prolog/session11.rkt
@@ -0,0 +1,30 @@
+#lang racklog
+
+nat(zero).
+nat(succ(X)) :- nat(X).
+
+
+likes(sam,Food) :-
+    indian(Food),
+    mild(Food).
+likes(sam,Food) :-
+    chinese(Food).
+likes(sam,Food) :-
+    italian(Food).
+likes(sam,chips).
+
+indian(curry).
+indian(dahl).
+indian(tandoori).
+indian(kurma).
+
+mild(dahl).
+mild(tandoori).
+mild(kurma).
+
+chinese(chow_mein).
+chinese(chop_suey).
+chinese(sweet_and_sour).
+
+italian(pizza).
+italian(spaghetti).
