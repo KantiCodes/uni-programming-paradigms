626d0982025e800e8c16cc9d04388333b897c5ca
eviluser <jendrekpb@gmail.com>
2022-01-03 16:16:15 +0100

    Finished session 3 and 4

--- haskell/session3/problem4.hs
+++ haskell/session3/problem4.hs
@@ -1,13 +1,16 @@
-data Maybe MyT = Just Int |Just Pair MyT MyT | Just Arrow MyT MyT  
-instance Show MyT where
-    show Int = "Int"
-    show (Arrow m n) = show m ++ "->" ++ show n
+data MyT = Int | Pair MyT MyT | Arrow MyT MyT | Maybe MyT deriving (Eq, Show)
 
 xt= ("name", Int)
 pairx= ("name-surname", Pair Int Int)
 my_env = [xt, pairx]
 
-
 get_by_key [] key = Nothing
-get_by_key ((currk,t):xs) key = if key == currk then t else (get_by_key xs key)
+get_by_key ((currk,t):xs) key = if key == currk then (Just t) else (get_by_key xs key)
 fun (x:xs) = if x == 3 then x else fun xs
+
+res = get_by_key my_env "namer"
+respar = get_by_key my_env "name-surname"
+
+
+upd (x,t) [] = [(x,t)]
+upd (x,t) ((y,t1):env1) = if (x==y) then ((x,t):env1) else (y,t1):(upd (x,t) env1)
